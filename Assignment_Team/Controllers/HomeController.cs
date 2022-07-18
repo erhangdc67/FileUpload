a7e1c2341971d877ebe11c1f6446aa0a381cafb3
Tesla <Tesla@ERHAN>
2022-07-18 23:47:24 +0300

    ss

--- Assignment_Team/Controllers/HomeController.cs
+++ Assignment_Team/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace Assignment_Team.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            return View();  
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
