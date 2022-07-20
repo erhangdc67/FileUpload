a518bdc13d98856dc35154bed73b3263eb951c50
erhangdc67 <kemalmemal67@gmail.com>
2022-07-20 07:49:53 +0300

    Update HomeController.cs

--- Assignment_Team/Controllers/HomeController.cs
+++ Assignment_Team/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using Assignment_Team.Models;
+using Assignment_Team.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +15,12 @@ namespace Assignment_Team.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -23,9 +28,28 @@ namespace Assignment_Team.Controllers
             return View();
         }
 
+        [HttpGet]
+        public JsonResult GetListInfo()
+        {
+            var Info = new List<FileInfomations>();
+
+            string contentRootPath = _webHostEnvironment.ContentRootPath;
+            string path = Path.Combine(contentRootPath, "UploadedFiles");
+
+            var filePath = path;
+
+            foreach (var file in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                FileInfo _fileInfo = new FileInfo(file);
+
+                Info.Add(new FileInfomations { Path = _fileInfo.FullName.ToString(),  Name = _fileInfo.Name, Lenght = _fileInfo.Length.ToString(), CreateDate = _fileInfo.CreationTime.ToString() });
+            }
+            return Json(Info);
+        }
+
         public IActionResult Privacy()
         {
-            return View();  
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
